2acd334c8d0f01f9e19dd3f6c65d6bd201bf3fa0
August Kliment <augustkliment94@gmail.com>
2025-11-08 16:00:39 -0600

    both lazy and normal

--- romanNumerals.js
+++ romanNumerals.js
@@ -10,8 +10,7 @@ function toRomanLazy(num) {
     D : 500, 
     M : 1000
   };
-  // console.log(romanNumeraltoArabic[])
-
+  
   const romanNumeralPriorityOrder = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
 
   for (let i = 0; i < romanNumeralPriorityOrder.length; i++){
@@ -32,19 +31,49 @@ function toRomanLazy(num) {
   }
   return output;
 }
-console.log(toRomanLazy(999))
+console.log(toRomanLazy(944))
 
 
-// for (let i = 1; i <= num; i++);
-// console.log(romanNumeralPriorityOrder[0])
+function toRoman(num) {
+  let output = "";
 
-// function toRomanLazy(num) {
-//   let output = "";
+  const romanNumeraltoArabic = {
+    I : 1, 
+    IV : 4,
+    V : 5, 
+    IX : 9,
+    X : 10,
+    XL : 40, 
+    L : 50, 
+    XC : 90,
+    C : 100,
+    CD : 400, 
+    D : 500, 
+    CM : 900,
+    M : 1000
+  };
+  // console.log(romanNumeraltoArabic[])
 
-// }
-// console.log(toRomanLazy(5))
-// function toRoman(num) {
-//   return "";
-// }
+  const romanNumeralPriorityOrder = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
-// module.exports = { toRoman, toRomanLazy };
+  for (let i = 0; i < romanNumeralPriorityOrder.length; i++){
+
+    const roman = romanNumeralPriorityOrder[i];
+
+    const arabic = romanNumeraltoArabic[roman];
+
+    const count = Math.floor(num / arabic);
+
+    output += roman.repeat(count);
+
+    num -= count * arabic;
+
+    if (num === 0) {
+      break;
+    }
+  }
+  return output;
+}
+console.log(toRoman(944))
+
+module.exports = { toRoman, toRomanLazy };
